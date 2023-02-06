1337e5f08459f45c6b2f30001ee3e47a6e26b353
VictorAlvaradoAraujo <viosalva@espol.edu.ec>
2023-02-06 10:10:10 -0500

    mod not

--- src/notificacion/IndiceNotificaciones.tsx
+++ src/notificacion/IndiceNotificaciones.tsx
@@ -11,11 +11,16 @@ import Paginacion from "utils/Paginacion";
 
  
 export default function IndiceNotificaciones() {
-    const [generos,setGeneros]= useState<rubricaDTO[]>();
+    const [rubricas,setRubricas]= useState<rubricaDTO[]>();
     const [totalDePaginas,setTotalDePaginas]=useState(0);
-    const [recordsPorPagina, setRecordsPorPagina]=useState(10);
+    const [recordsPorPagina, setRecordsPorPagina]=useState(5);
     const [pagina,setPagina]=useState(1);
+
     const [rutas,setRutas]= useState<rutaDTO[]>();
+    const [totalDePaginas2,setTotalDePaginas2]=useState(0);
+    const [recordsPorPagina2, setRecordsPorPagina2]=useState(5);
+    const [pagina2,setPagina2]=useState(1);
+    
     useEffect(()=>{
         cargarDatos();
     //eslint-disable-next-line react-hooks/exhaustive-deps
@@ -30,7 +35,7 @@ export default function IndiceNotificaciones() {
                 parseInt(respuesta.headers['cantidadtotalregistros'],10);
             setTotalDePaginas(Math.ceil(totalDeRegistros/recordsPorPagina));
             console.log(respuesta.data);
-            setGeneros(respuesta.data);
+            setRubricas(respuesta.data);
         })
     }
 
@@ -38,15 +43,15 @@ export default function IndiceNotificaciones() {
         cargarDatosRutas();
     //eslint-disable-next-line react-hooks/exhaustive-deps
 
-    },[pagina,recordsPorPagina])
+    },[pagina2,recordsPorPagina2])
     function cargarDatosRutas(){
         axios.get(urlRutas,{
-            params: {pagina,recordsPorPagina: recordsPorPagina}
+            params: {pagina2,recordsPorPagina2: recordsPorPagina2}
         })
         .then((respuesta:AxiosResponse<rutaDTO[]>)=>{
-            const totalDeRegistros= 
+            const totalDeRegistros2= 
                 parseInt(respuesta.headers['cantidadtotalregistros'],10);
-            setTotalDePaginas(Math.ceil(totalDeRegistros/recordsPorPagina));
+            setTotalDePaginas2(Math.ceil(totalDeRegistros2/recordsPorPagina2));
             console.log(respuesta.data);
             setRutas(respuesta.data);
         })
@@ -55,61 +60,55 @@ export default function IndiceNotificaciones() {
 
 return (
         <div className="container col-lg-9">
-        
             <h3>Notificaciones</h3>
-
- 
- 
-     
-            <ListadoGenerico listado={generos}>
+            <ListadoGenerico listado={rubricas}>
             <Grid className="display: 'flex',flexDirection: 'column', alignItems: 'center', direction: 'column', justify: 'space-between'">
                     <Card sx={{ marginTop:10 }}>
                     <CardContent sx={{ paddingY: 5, paddingX: 1 }}>
-                      
                         <table className="table">
                             <thead>
                                 <tr className="color">
                                     <th>Estado de Rúbricas</th>
                                 </tr>
-                               
                             </thead>
                             <tbody>
-                                {generos?.map(genero=>
-                                    <tr key={genero.id}>
-                                
+                                {rubricas?.map(rubrica=>
+                                    <tr key={rubrica.id}>
                                         <td>{(() => {
-                                    switch (genero.estado) {
+                                    switch (rubrica.estado) {
                                     case "":   return <Alert severity="info">
                                                        <AlertTitle><strong>Pendiente</strong></AlertTitle>
-                                                            La rúbrica <strong>{genero.nombre}</strong>
+                                                            La rúbrica <strong>{rubrica.nombre}</strong>
                                                         </Alert>;
                                     case "Pendiente": return <Alert severity="info">
                                                         <AlertTitle><strong>Pendiente</strong></AlertTitle>
-                                                            La rúbrica <strong>{genero.nombre}</strong>
+                                                            La rúbrica <strong>{rubrica.nombre}</strong>
                                                         </Alert>;
                                     case "Aprobado":  return <Alert severity="success"onClose={() => {}}>
                                                              <AlertTitle><strong>Aprobada</strong></AlertTitle>
-                                                            La rúbrica <strong>{genero.nombre}</strong></Alert>;
+                                                            La rúbrica <strong>{rubrica.nombre}</strong></Alert>;
                                     case "Rechazado":  return <Alert severity="error">
                                                                 <AlertTitle><strong>Rechazada</strong></AlertTitle>
-                                                            La rúbrica <strong>{genero.nombre}</strong></Alert>;
+                                                            La rúbrica <strong>{rubrica.nombre}</strong></Alert>;
                                     default:      return <Alert severity="info">
                                                         <AlertTitle><strong>Pendiente</strong></AlertTitle>
-                                                            La rúbrica <strong>{genero.nombre}</strong>
+                                                            La rúbrica <strong>{rubrica.nombre}</strong>
                                                         </Alert>;
                                     }
                                 })()}</td>
-                        
                                     </tr>)}
                             </tbody>
                         </table>
-
-
                     </CardContent>
-                    <ListadoGenerico listado={rutas}>
+                    <Paginacion cantidadTotalDePaginas={totalDePaginas}
+            paginaActual={pagina} onChange={nuevaPagina=> setPagina(nuevaPagina)}/>
+                    </Card>
+             </Grid> 
+            </ListadoGenerico>
+            <ListadoGenerico listado={rutas}>
             <Grid className="display: 'flex',flexDirection: 'column', alignItems: 'center', direction: 'column', justify: 'space-between'">
                     <Card sx={{ marginTop:10 }}>
-                    <CardContent sx={{ paddingY: 5, paddingX: 1 }}>
+                    <CardContent sx={{ paddingY: 3, paddingX: 1 }}>
                       
                         <table className="table">
                             <thead>
@@ -151,18 +150,11 @@ return (
 
 
                     </CardContent>
- 
+                    <Paginacion cantidadTotalDePaginas={totalDePaginas2}
+            paginaActual={pagina2} onChange={nuevaPagina2=> setPagina2(nuevaPagina2)}/>
                     </Card>
              </Grid> 
             </ListadoGenerico>
-                                
-                    <Paginacion cantidadTotalDePaginas={totalDePaginas}
-            paginaActual={pagina} onChange={nuevaPagina=> setPagina(nuevaPagina)}/>
-                    </Card>
-             </Grid> 
-            </ListadoGenerico>
-
-            
  
         </div>
 
